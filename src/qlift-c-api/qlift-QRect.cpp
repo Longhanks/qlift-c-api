c8c8de92f7fbf83e5df6c2ff39e22bdc884015c2
Andreas Schulz <andi.schulz@me.com>
2017-08-27 17:43:16 +0200

    QRect: implement important properties

--- src/qlift-c-api/qlift-QRect.cpp
+++ src/qlift-c-api/qlift-QRect.cpp
@@ -12,3 +12,40 @@ void QRect_delete(void *rect) {
     delete static_cast<QRect*>(rect);
 }
 
+void* QRect_center(void *rect) {
+    auto stackPoint = static_cast<QRect*>(rect)->center();
+    return static_cast<void*>(new QPoint {stackPoint.x(), stackPoint.y()});
+}
+
+int QRect_x(void *rect) {
+    return static_cast<QRect*>(rect)->x();
+}
+
+int QRect_y(void *rect) {
+    return static_cast<QRect*>(rect)->y();
+}
+
+void QRect_setX(void *rect, int x) {
+    static_cast<QRect*>(rect)->setX(x);
+}
+
+void QRect_setY(void *rect, int y) {
+    static_cast<QRect*>(rect)->setY(y);
+}
+
+int QRect_height(void *rect) {
+    return static_cast<QRect*>(rect)->height();
+}
+
+int QRect_width(void *rect) {
+    return static_cast<QRect*>(rect)->width();
+}
+
+void QRect_setHeight(void *rect, int height) {
+    static_cast<QRect*>(rect)->setHeight(height);
+}
+
+void QRect_setWidth(void *rect, int width) {
+    static_cast<QRect*>(rect)->setHeight(width);
+}
+
--- src/qlift-c-api/qlift-QRect.h
+++ src/qlift-c-api/qlift-QRect.h
@@ -11,6 +11,15 @@ extern "C" {
 
 void* QRect_new(int x, int y, int width, int height);
 void QRect_delete(void *rect);
+void* QRect_center(void *rect);
+int QRect_x(void *rect);
+int QRect_y(void *rect);
+void QRect_setX(void *rect, int x);
+void QRect_setY(void *rect, int y);
+int QRect_height(void *rect);
+int QRect_width(void *rect);
+void QRect_setHeight(void *rect, int height);
+void QRect_setWidth(void *rect, int width);
 
 #ifdef __cplusplus
 }
