dc36b79da6f63b524441976985b58de6a04c1432
Andreas Schulz <andi.schulz@me.com>
2017-09-27 20:09:10 +0200

    QPushButton: mousePressEvent

--- src/qlift-c-api/qlift-QPushButton.cpp
+++ src/qlift-c-api/qlift-QPushButton.cpp
@@ -1,4 +1,5 @@
 #include <QPushButton>
+#include <wobjectimpl.h>
 
 #define QLIFT_C_API_INTERNAL
 #include "qlift-QPushButton.h"
@@ -6,12 +7,36 @@
 
 void* QPushButton_new(void *icon, const char *text, void *parent) {
     if (icon) {
-        return static_cast<void*>(new QPushButton {*static_cast<QIcon*>(icon), text, static_cast<QWidget*>(parent)});
+        return static_cast<void*>(new QliftPushButton {*static_cast<QIcon*>(icon), text, static_cast<QWidget*>(parent)});
     }
-    return static_cast<void*>(new QPushButton {text, static_cast<QWidget*>(parent)});
+    return static_cast<void*>(new QliftPushButton {text, static_cast<QWidget*>(parent)});
 }
 
 void QPushButton_delete(void *pushButton) {
-    delete static_cast<QPushButton*>(pushButton);
+    delete static_cast<QliftPushButton*>(pushButton);
+}
+
+void QPushButton_mousePressEvent(void *pushButton, void *mouseEvent) {
+    static_cast<QliftPushButton*>(pushButton)->mousePressEventSuper(static_cast<QMouseEvent*>(mouseEvent));
+}
+
+void QPushButton_mousePressEvent_Override(void *pushButton, void *context, void (*mousePressEvent_Functor)(void*, void*)) {
+    static_cast<QliftPushButton*>(pushButton)->mousePressEvent_Functor = mousePressEvent_Functor;
+    static_cast<QliftPushButton*>(pushButton)->context = context;
+}
+
+W_OBJECT_IMPL(QliftPushButton)
+
+void QliftPushButton::mousePressEventSuper(QMouseEvent *e) {
+    QPushButton::mousePressEvent(e);
+}
+
+void QliftPushButton::mousePressEvent(QMouseEvent *e) {
+    if (mousePressEvent_Functor) {
+        (*mousePressEvent_Functor)(context, e);
+    }
+}
+
+QliftPushButton::~QliftPushButton() {
 }
 
--- src/qlift-c-api/qlift-QPushButton.h
+++ src/qlift-c-api/qlift-QPushButton.h
@@ -11,9 +11,28 @@ extern "C" {
 
 void* QPushButton_new(void *icon, const char *text, void *parent);
 void QPushButton_delete(void *pushButton);
+void QPushButton_mousePressEvent(void *pushButton, void *mouseEvent);
+void QPushButton_mousePressEvent_Override(void *pushButton, void *context, void (*mousePressEvent_Functor)(void*, void*));
 
 #ifdef __cplusplus
 }
+#endif
+
+#ifdef __cplusplus
+#include <wobjectdefs.h>
+class QliftPushButton: public QPushButton {
+    W_OBJECT(QliftPushButton)
+
+    public:
+        using QPushButton::QPushButton;
+        virtual ~QliftPushButton();
+        void (*mousePressEvent_Functor)(void*, void*) = nullptr;
+        void *context = nullptr;
+        void mousePressEventSuper(QMouseEvent *e);
+
+    protected:
+        virtual void mousePressEvent(QMouseEvent *e) override;
+};
 #endif
 
 #endif //QLIFT_C_API_QLIFT_QPUSHBUTTON_H
