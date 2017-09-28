e0099b10b6ba32623d7528ebfa45d47bec7ed21c
Andreas Schulz <andi.schulz@me.com>
2017-09-28 21:02:14 +0200

    QMainWindow: closeEvent

--- src/qlift-c-api/qlift-QMainWindow.cpp
+++ src/qlift-c-api/qlift-QMainWindow.cpp
@@ -1,14 +1,15 @@
 #include <QMainWindow>
+#include <wobjectimpl.h>
 
 #include "qlift-QMainWindow.h"
 
 
 void* QMainWindow_new(void *parent, int flags) {
-    return static_cast<void*>(new QMainWindow {static_cast<QWidget*>(parent), static_cast<QFlags<Qt::WindowType>>(flags)});
+    return static_cast<void*>(new QliftMainWindow {static_cast<QWidget*>(parent), static_cast<QFlags<Qt::WindowType>>(flags)});
 }
 
 void QMainWindow_delete(void *mainWindow) {
-    delete static_cast<QMainWindow*>(mainWindow);
+    delete static_cast<QliftMainWindow*>(mainWindow);
 }
 
 void* QMainWindow_centralWidget(void *mainWindow) {
@@ -25,5 +26,29 @@ void* QMainWindow_menuBar(void *mainWindow) {
 
 void QMainWindow_setMenuBar(void *mainWindow, void *menuBar) {
     static_cast<QMainWindow*>(mainWindow)->setMenuBar(static_cast<QMenuBar*>(menuBar));
+}
+
+void QMainWindow_closeEvent(void *mainWindow, void *event) {
+    static_cast<QliftMainWindow*>(mainWindow)->closeEventSuper(static_cast<QCloseEvent*>(event));
+}
+
+void QMainWindow_closeEvent_Override(void *mainWindow, void *context, void (*closeEvent_Functor)(void*, void*)) {
+    static_cast<QliftMainWindow*>(mainWindow)->closeEvent_Functor = closeEvent_Functor;
+    static_cast<QliftMainWindow*>(mainWindow)->context = context;
+}
+
+W_OBJECT_IMPL(QliftMainWindow)
+
+void QliftMainWindow::closeEventSuper(QCloseEvent *e) {
+    QMainWindow::closeEvent(e);
+}
+
+void QliftMainWindow::closeEvent(QCloseEvent *e) {
+    if (closeEvent_Functor) {
+        (*closeEvent_Functor)(context, e);
+    }
+}
+
+QliftMainWindow::~QliftMainWindow() {
 }
 
--- src/qlift-c-api/qlift-QMainWindow.h
+++ src/qlift-c-api/qlift-QMainWindow.h
@@ -11,9 +11,28 @@ void* QMainWindow_centralWidget(void *mainWindow);
 void QMainWindow_setCentralWidget(void *mainWindow, void *widget);
 void* QMainWindow_menuBar(void *mainWindow);
 void QMainWindow_setMenuBar(void *mainWindow, void *menuBar);
+void QMainWindow_closeEvent(void *mainWindow, void *event);
+void QMainWindow_closeEvent_Override(void *mainWindow, void *context, void (*closeEvent_Functor)(void*, void*));
 
 #ifdef __cplusplus
 }
+#endif
+
+#ifdef __cplusplus
+#include <wobjectdefs.h>
+class QliftMainWindow: public QMainWindow {
+    W_OBJECT(QliftMainWindow)
+
+    public:
+        using QMainWindow::QMainWindow;
+        virtual ~QliftMainWindow();
+        void (*closeEvent_Functor)(void*, void*) = nullptr;
+        void *context = nullptr;
+        void closeEventSuper(QCloseEvent *e);
+
+    protected:
+        virtual void closeEvent(QCloseEvent *e) override;
+};
 #endif
 
 #endif //QLIFT_C_API_QLIFT_QMAINWINDOW_H
