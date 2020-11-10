b34301678b3239d2518564863a61799469f57a0c
Andreas Schulz <andi.schulz@me.com>
2020-11-10 20:33:47 +0100

    Fix package name

--- Package.swift
+++ Package.swift
@@ -1,10 +1,8 @@
 // swift-tools-version:5.3
-
 import PackageDescription
 
-
 let package = Package(
-    name: "Qlift",
+    name: "CQlift",
     products: [
         .library(
             name: "CQlift",
